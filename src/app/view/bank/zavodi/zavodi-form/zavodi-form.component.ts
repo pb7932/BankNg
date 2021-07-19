103b2c6adc1a8879388c21fef61a8b776c2c5e46
pb7932 <pebe1928@gmail.com>
2021-07-19 17:46:17 +0200

    Implementing zavodi form component

--- src/app/view/bank/zavodi/zavodi-form/zavodi-form.component.ts
+++ src/app/view/bank/zavodi/zavodi-form/zavodi-form.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ZavodDTO, ZavodiHelper, ZavodRequestDTO } from 'src/app/model/bank/zavodi/zavodi';
+import { MyDataService } from 'src/app/services/my-data.service';
+import { MyBaseFormComponent } from 'src/app/shared/form/my-base-form-component';
 
 @Component({
   selector: 'app-zavodi-form',
   templateUrl: './zavodi-form.component.html',
   styleUrls: ['./zavodi-form.component.css']
 })
-export class ZavodiFormComponent implements OnInit {
+export class ZavodiFormComponent extends MyBaseFormComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private routerForm1: Router,
+    private routeForm1: ActivatedRoute,
+    private myDataService1: MyDataService,
+  ) { 
+    super(routerForm1, routeForm1, myDataService1, ZavodiHelper.routeName)
   }
 
+  myNgOnInit() { }
+
+  initForm() {
+    this.item = new ZavodRequestDTO();
+    this.item.data = new ZavodDTO();
+  }
+
+  fetchDataOK() { }
+
+  validate() {
+    return true;
+  }
 }
--- src/app/view/bank/zavodi/zavodi-form/zavodi-form.module.ts
+++ src/app/view/bank/zavodi/zavodi-form/zavodi-form.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ZavodiFormRoutingModule } from './zavodi-form-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ZavodiFormComponent } from './zavodi-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ZavodiFormComponent
+  ],
   imports: [
     CommonModule,
     FormsModule,
